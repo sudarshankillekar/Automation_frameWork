34bdda1141022cc7359d59cf5d6cd2be3bc8b91e
sudarshan <sudarshankillekar1998@gmail.com>
2024-04-01 23:52:34 +0530

    test

---
+++ src/test/java/com/ui/pages/loginpage.java
@@ -0,0 +1,26 @@
+package com.ui.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import com.util.BrowserUtility;
+
+public final class loginpage extends BrowserUtility{
+	
+	private static final By USERNAMETEXRBOXLOCATOR = By.id("username");
+	private static final By PASSWORDTEXRBOXLOCATOR = By.id("password");
+	private static final By LOGINBUTTONLOCATOR = By.xpath("//span[contains(text(),\" Sign in \")]/../..");
+	
+	public loginpage(WebDriver wd) {
+		super(wd);
+		goToWebSite("http://phoenix.testautomationacademy.in/sign-in");
+	}
+    
+	public void dologinwith(String userName, String password) {
+		clearText(USERNAMETEXRBOXLOCATOR);		
+		enterTextInto(USERNAMETEXRBOXLOCATOR, userName);
+		clearText(PASSWORDTEXRBOXLOCATOR);
+		enterTextInto(PASSWORDTEXRBOXLOCATOR, password);
+		clickOn(LOGINBUTTONLOCATOR);		
+	}
+	
+}
--- src/test/java/com/util/BrowserUtility.java
+++ src/test/java/com/util/BrowserUtility.java
@@ -18,7 +18,7 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class BrowserUtility {
+public abstract class BrowserUtility {
 
 	private WebDriver wd;
 	private WebDriverWait wait;
